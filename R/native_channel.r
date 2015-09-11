5017a761ef3fc2a5cf1f0ee43680805c6af26383
Alfonso <alfcrisci@gmail.com>
2015-09-11 20:48:27 +0200

    Update native_channel.r

--- R/native_channel.r
+++ R/native_channel.r
@@ -2,7 +2,7 @@
 #'
 #' @description extract a channel object only with original tweet 
 #'
-#' @param  channel  data.frame  Channel object
+#' @param  channel  data.frame  channel object
 #' @return channel_obj data.frame Return a data.frame 
 #' @author  Istituto di Biometeorologia Firenze Italy  Alfonso Crisci \email{a.crisci@@ibimet.cnr.it}
 #' @keywords  channel
@@ -13,9 +13,9 @@
 #'
 #'
 
-native_channel=function(channel) {
+native_channel = function(channel) {
   
-  ls_retweet=unlist(lapply(x$text,FUN=function(x) is.retweet(x)))
-  channel_obj=channel[which(ls_retweet==FALSE),]
+  ls_retweet = unlist(lapply(x$text,FUN=function(x) is.retweet(x)))
+  channel_obj = channel[which(ls_retweet==FALSE),]
   return(channel_obj)
 }
