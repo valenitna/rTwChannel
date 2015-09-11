b36b261e21cf16165c74f08a2fd60fd205424432
Alfonso <alfcrisci@gmail.com>
2015-09-11 17:33:02 +0200

    Update stat_notes.r

--- R/stat_notes.r
+++ R/stat_notes.r
@@ -14,37 +14,36 @@
 #'
 #'
 
-stat_notes=function(x,notes) {
-  
-  require(qdap)
-  require(qdapTools)
-  options( warn = -1)
-  x[,2]=tolower(x[,2])
-  label_notes=notes$y[1]
-  x_unique=unique(x)
-  elements=gsub("^@","",x[,2])
-  elements_unique=gsub("^@","",x_unique[,2])
-  mapnotes=notes[elements]
-  mapnotes_unique=notes[elements_unique]
-  mapnotes$data=x$data
-  mapnotes_unique$data=x_unique$data
-  mapnotes=na.omit(mapnotes)
-  mapnotes_unique=na.omit(mapnotes_unique)
-  freq_day=as.data.frame(as.matrix(table(mapnotes$data)))
-  names(freq_day)=c("N")
-  freq_day$data=rownames(freq_day)
-  rownames(freq_day)<-NULL
-  freq_day_unique=as.data.frame(as.matrix(table(mapnotes_unique$data)))
-  names(freq_day_unique)=c("N")
-  freq_day_unique$data=rownames(freq_day_unique)
-  rownames(freq_day_unique)<-NULL
-  res=list()
-  res$df=data.frame(freq_day$data,freq_day$N)
-  res$df_unique=data.frame(freq_day_unique$data,freq_day_unique$N)
-  names(res$df)<-c("date",label_notes)
-  names(res$df_unique)<-c("date",label_notes)
-  res$N=nrow(na.omit(mapnotes))
-  res$N_sumuniquedate=nrow(na.omit(mapnotes_unique))
-  return(res)
-  options(warn=0)
+stat_notes=function (x, notes) 
+{
+    require(qdap)
+    require(qdapTools)
+    options(warn = -1)
+    x[,2] = tolower(x[,2])
+    x[,2] = gsub("^@", "", x[, 2])
+    label_notes = notes$y[1]
+    x_unique = unique(x[,1:2])
+    terms=x[,2]
+    terms_unique=x_unique[,2]
+    mapnotes=data.frame(data=x[,1],note_target=terms,authors=x$authors,noteterms=notes[terms]$y)
+    mapnotes_unique =data.frame(data=x_unique[,1],note_target=terms_unique,noteterms=notes[terms_unique]$y)
+    mapnotes = na.omit(mapnotes)
+    mapnotes_unique = na.omit(mapnotes_unique)
+    freq_day = as.data.frame(as.matrix(table(mapnotes$data)))
+    names(freq_day) = c("N")
+    freq_day$data = rownames(freq_day)
+    freq_day_unique = as.data.frame(as.matrix(table(mapnotes_unique$data)))
+    names(freq_day_unique) = c("N")
+    freq_day_unique$data = rownames(freq_day_unique)
+    res = list()
+    res$df = data.frame(data=freq_day$data,N= freq_day$N)
+    res$df_unique = data.frame(data=freq_day_unique$data,N=freq_day_unique$N)
+    names(res$df) <- c("date", label_notes)
+    names(res$df_unique) <- c("date", label_notes)
+    res$N = nrow(mapnotes)
+    res$N_sumuniquedate = nrow(mapnotes_unique)
+    res$mapnotes= mapnotes
+    res$mapnotes_unique= mapnotes_unique
+    return(res)
+    options(warn = 0)
 }
