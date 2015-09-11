0513896ae1f6821dc2e009172c052349d73919ea
Alfonso <alfcrisci@gmail.com>
2015-09-11 20:47:45 +0200

    Update create_stat_note_obj.r

--- R/create_stat_note_obj.r
+++ R/create_stat_note_obj.r
@@ -16,16 +16,16 @@
 #'
 #'
 
-create_state_obj=function(x,notes,save=TRUE,fileName="") {
+create_state_obj = function(x,notes,save=TRUE,fileName="") {
   
   res=list()
 
   for ( i in seq_along(notes))
-  { notes_temp=notes[[i]]
-    notes_temp=notes_temp[!duplicated(notes_temp),]
-    res[[i]]=stat_notes(x,notes_temp)
+  { notes_temp = notes[[i]]
+    notes_temp = notes_temp[!duplicated(notes_temp),]
+    res[[i]] = stat_notes(x,notes_temp)
   }
-  if ( save ==TRUE) {
+  if ( save == TRUE) {
                     saveRDS(res,file=paste0(fileName,".rds"))
   }
   return(res)
