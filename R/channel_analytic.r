7eee9425c6f7eebf80e424dd2e9932da002dcad5
Alfonso <alfcrisci@gmail.com>
2015-10-13 17:24:31 +0200

    Update channel_analytic.r

--- R/channel_analytic.r
+++ R/channel_analytic.r
@@ -206,7 +206,7 @@ channel_analytic=function(channel_obj,use_channel_dates=TRUE, start_date=NULL, e
   ls_lenlinks=unlist(lapply( ls_links,FUN=function(x) ifelse(is.na(x),0, length(qdapRegex::rm_url(x, extract=TRUE)[[1]]))))
   ls_lentag=unlist(lapply(ls_tag,FUN=function(x) ifelse(is.na(x),0, length(extract_mentions(x)[[1]]))))
   ls_words=unlist(lapply(channel_obj$text,FUN=function(x) qdap::word_count(x)))
-  ls_retweeted_authors=lapply(channel_obj$text,FUN=function(x) return(qdapRegex::rm_default(x, pattern="RT @([:alnum:]*[_]*[:alnum:]*):",extract=T)));
+  ls_retweeted_authors=lapply(channel_obj$text,FUN=function(x) return(stringr::str_extract((x, pattern="RT @([:alnum:]*[_]*[:alnum:]*):",extract=T)));
   ls_retweeted_authors=unlist(lapply(ls_retweeted_authors,function(x) gsub(":","",gsub("^RT @","",x[1]))))
   write.csv(ls_retweeted_authors,"ls_retweeted_authors.csv",row.names=F)
  
@@ -215,9 +215,10 @@ channel_analytic=function(channel_obj,use_channel_dates=TRUE, start_date=NULL, e
   #######################################################################################
   # Create data.frame date,retweeted_authors and authors.
                          
-  ls_retweeted_df=data.frame(data=channel_obj$data,
-                             retweeted_authors=as.character(ls_retweeted_authors),
-                             authors=channel_obj$screeName)
+  ls_retweeted_df=na.omit(data.frame(data=channel_obj$data,
+                             retweeted_authors=ls_retweeted_authors,
+                             authors=channel_obj$screeName))
+                             
   write.csv(ls_retweeted_df,"ls_retweeted_df.csv",row.names=F)
  
   ####################################################################################
